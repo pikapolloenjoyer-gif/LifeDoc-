b9f5f33f817ad17c7f40ae4fc77024d49bae2828
pikapolloenjoyer-gif <carlosj.abreureyes@gmail.com>
2026-07-31 08:43:09 -0400

    feat: Agregar menú para actualizar o borrar días

--- LifeDOC.py
+++ LifeDOC.py
@@ -38,8 +38,12 @@ pushups = input("¿Hiciste push-ups? (Sí/No): ")
 jugar = input("¿Jugaste? (Sí/No): ")
 leer = input("¿Leíste? (Sí/No): ")
 
+# volver = input("Quieres volver días atras? (Sí/No): ")
 
-to_do_list[ayer.strftime("%d")] = {
+# if volver.lower() == "sí":
+#    dias_atras = input("¿A que día quieres volver atras?")
+
+to_do_list[ayer.strftime("%d", dias[ayer.weekday()])] = {
    "highlight": highlight,
    "madrugar": madrugar,
    "limpieza": limpieza,
@@ -57,18 +61,57 @@ tabla.add_column("Push-ups", justify="center")
 tabla.add_column("Jugar", justify="center")
 tabla.add_column("Leer", justify="center")
 
-
-
 os.system("cls") 
 for key, value in to_do_list.items():
     
  tabla.add_row(f"{key}", value["highlight"], value["madrugar"], value["limpieza"], value["coding"], value["pushups"], value["jugar"], value["leer"])
-  
-    
-
-
+   
 console = Console()
 console.print(tabla)
+
+while True:
+  opcion = input("\n1. Actualizar  \n2.Eliminar \n3.Salir \nIngresa una opción: ")
+
+  if opcion == 1:
+    if not to_do_list:
+      print("No hay días registrados")
+      continue
+
+    to_do_list[ayer.strftime("%d", dias[ayer.weekday()])] = input("Ingresa el dia")
+    if not dias in to_do_list.keys:
+      print("Ingresa un día valido")
+      continue
+    highlight = input("Highlight del día: ")
+    madrugar = input("¿Madrugaste? (Sí/No): ")
+    limpieza = input("¿Hiciste limpieza? (Sí/No): ")
+    coding = input("Coding? (Sí/No): ")
+    pushups = input("¿Hiciste push-ups? (Sí/No): ")
+    jugar = input("¿Jugaste? (Sí/No): ")
+    leer = input("¿Leíste? (Sí/No): ")
+
+    to_do_list[dias] = {
+      "highlight": highlight,
+      "madrugar": madrugar,
+      "limpieza": limpieza,
+      "coding": coding,
+      "pushups": pushups,
+      "jugar": jugar,
+      "leer": leer
+    }
+
+    if not (highlight and madrugar and limpieza and coding and pushups and jugar and leer):
+      print("No se agregaron los datos correctamente")
+      continue
+    print("Se actualizaron los datos correctamente")
+    continue
+  elif opcion == 3:
+    break
+  
+    
+      
+    
+
+   
 
 with open("To do list.json", "w") as f:
     json.dump(to_do_list, f)
